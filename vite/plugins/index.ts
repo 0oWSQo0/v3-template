847293d22cfca7b93e410f334ff4f87ad1111cb3
WSQ <592786982@qq.com>
2023-12-12 09:12:32 +0800

    update

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -6,6 +6,7 @@ import createCompression from './compression'
 import createSetupExtend from './setup-extend'
 import createSvgIcon from './svg-icon'
 import Icons from 'unplugin-icons/vite'
+import electron from 'vite-plugin-electron'
 
 export default function createVitePlugins(viteEnv: Record<string, string>, isBuild = false) {
   const vitePlugins: PluginOption[] = [
@@ -14,7 +15,10 @@ export default function createVitePlugins(viteEnv: Record<string, string>, isBui
     createComponents(),
     createSetupExtend(),
     createSvgIcon(isBuild),
-    Icons({ autoInstall: true })
+    Icons({ autoInstall: true }),
+    electron({
+      entry: 'electron/main.ts'
+    })
   ]
   isBuild && vitePlugins.push(...createCompression(viteEnv))
   return vitePlugins
