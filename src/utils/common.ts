6c07581e71ad0b3ea1cbc7d465f05d136f8b3aaa
WSQ <592786982@qq.com>
2024-03-07 16:19:50 +0800

    ttt
    Signed-off-by: WSQ <592786982@qq.com>

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -156,5 +156,6 @@ export function getNormalPath(p: any) {
 
 // 验证是否为blob格式
 export function blobValidate(data: any) {
+  console.log(`output->data`,data)
   return data.type !== 'application/json'
 }
