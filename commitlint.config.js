d3d3a03e3d1507951349dd0c10f0e668905395b0
WSQ <592786982@qq.com>
2024-03-07 18:09:32 +0800

    🚀 新增: sfds

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['cz'],
   rules: {
-    'type-empty': [0], //不允许提交的类型为空
-    'subject-empty': [0] //不允许提交的内容为空
+    'type-empty': [0, 'never'], //不允许提交的类型为空
+    'subject-empty': [0, 'never'] //不允许提交的内容为空
   }
 }
