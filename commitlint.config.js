752409a8b0757d3f5008d1c05f8eadd7b58352f0
WSQ <592786982@qq.com>
2024-03-08 09:19:25 +0800

    fix: xxx
    Signed-off-by: WSQ <592786982@qq.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'subject-empty': [2, 'never'],
-  'type-empty': [2, 'never'],
   rules: {
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
     'type-enum': [
       2,
       'always',
